38520c34ae2f4a370d4d13d9584c42703bc29374
chintoz <jacintomena@gmail.com>
2020-10-11 01:38:16 +0200

    Changes to add kafka stuff to publish simple messages.

--- src/test/scala/es/menasoft/gatling/simulations/FirstSimulation.scala
+++ src/test/scala/es/menasoft/gatling/simulations/FirstSimulation.scala
@@ -1,11 +1,14 @@
 package es.menasoft.gatling.simulations
 
+import java.lang.System.currentTimeMillis
+
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
 import io.gatling.http.request.builder.HttpRequestBuilder.toActionBuilder
-
+import com.github.mnogu.gatling.kafka.Predef._
+import org.apache.kafka.clients.producer.ProducerConfig
 
 class FirstSimulation extends Simulation {
 
@@ -17,14 +20,37 @@ class FirstSimulation extends Simulation {
     println("[JJML] Simulation ends")
   }
 
+  val kafkaConf = kafka
+    .topic("prueba")
+    .properties(
+      Map(
+        ProducerConfig.ACKS_CONFIG -> "1",
+        // list of Kafka broker hostname and port pairs
+        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9092",
+        // in most cases, StringSerializer or ByteArraySerializer
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG ->
+          "org.apache.kafka.common.serialization.StringSerializer",
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG ->
+          "org.apache.kafka.common.serialization.StringSerializer"))
+
+  /*
   val httpProtocolBuilder: HttpProtocolBuilder = http
     .baseUrl("http://localhost:8080")
+   */
 
+  /*
   val scenarioBuilder: ScenarioBuilder = scenario("First Scenario")
     .exec(
       http("Count Request")
         .get("/messages")
     )
+   */
 
-  setUp(scenarioBuilder.inject(atOnceUsers(1000))).protocols(httpProtocolBuilder)
+  val scenarioBuilder: ScenarioBuilder = scenario("First Scenario Test")
+    .exec(
+      kafka("Count Messages")
+        // message to send
+        .send[String]("Example Message " + currentTimeMillis()))
+
+  setUp(scenarioBuilder.inject(atOnceUsers(1000000))).protocols(kafkaConf)
 }
